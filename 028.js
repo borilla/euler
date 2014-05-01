d17f711c9d983bd3d76621d347063344a2c83cc2
borilla <john@borilla.co.uk>
2014-05-01 13:46:23 +0100

    Problem 28

--- 028.js
+++ 028.js
@@ -1,11 +1,8 @@
 function f(x) {
-	if (x == 1) {
-		return 1;
-	}
-	// else
-	return (4 * x * x) - (6 * x) + 6 + f(x - 2)
+	return (x == 1) ? 1 : (4 * x * x) - (6 * x) + 6 + f(x - 2)
 }
 
-console.log(f(1));
-console.log(f(5));
-console.log(f(1001));
+console.log(f(1)); // 1
+console.log(f(3)); // 25
+console.log(f(5)); // 101
+console.log(f(1001)); // 669171001

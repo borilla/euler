d461517497c36784f3dbf5d591cdcd641e8b2539
John Adams <github@borilla.co.uk>
2014-05-24 20:37:08 +0100

    Updated problem 12 to use new "factors" functionality of Primes

--- 012.js
+++ 012.js
@@ -1,14 +1,22 @@
+var target = 500;
+
+var t1 = Date.now();
+
 var n = 0;
 var triangle = 0;
-var factors = [];
-
+var count;
 do {
 	n += 1;
 	triangle = triangle + n;
-	factors = Factors.allFactors(triangle);
-	console.log(triangle, factors.length, factors);
-} while (factors.length < 20);
+	count = Primes.countFactors(triangle);
+} while (count < target);
 
-triangle = 76576500;
-factors = Factors.allFactors(triangle);
-console.log(triangle, factors.length, factors);
+var t2 = Date.now();
+
+// triangle = 76576500;
+
+console.log('time: ' + (t2 - t1) + 'ms');
+console.log('triangle number: ' + triangle);
+console.log('count of factors: ' + count);
+console.log('prime factors:', Primes.getPrimeFactors(triangle));
+console.log('all factors:', Primes.getAllFactors(triangle));

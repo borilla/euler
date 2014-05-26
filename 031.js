b5dd107a9f2e99d68bae9d9ccf9f5e336af45bb2
John Adams <github@borilla.co.uk>
2014-05-26 05:09:49 +0100

    Problem 31 - using solution from stackoverflow

--- 031.js
+++ 031.js
@@ -1,47 +1,18 @@
 var coins = [1,2,5,10,20,50,100,200];
 
-function same(a1, a2) {
-	if (a1.length != a2.length) {
-		return false;
-	}
-	for (var i = 0, l = a1.length; i < l; ++i) {
-		if (a1[i] != a2[i]) {
-			return false;
+// http://stackoverflow.com/questions/4243831/how-to-count-possible-combination-for-coin-problem
+// using dynamic programming (dp) to get increasingly complex results from simple starting state
+// // http://community.topcoder.com/tc?module=Static&d1=tutorials&d2=dynProg
+function findCombinationsCount(sum, coins) {
+	var dp = [1];
+
+	coins.forEach(function(coin) {
+		for (var i = coin; i <= sum; ++i) {
+			dp[i] = (dp[i] || 0) + dp[i - coin];
 		}
-	}
-	return true;
+	});
+
+	return dp[sum];
 }
 
-function unique(a) {
-	a.forEach(function(a) {
-		a.sort();
-	})
-	a.sort();
-
-	var result = [];
-	for (var i = 0, l = a.length; i < l; ++i) {
-		if (i == 0 || !(same(a[i], a[i - 1]))) {
-			result.push(a[i]);
-		}
-	}
-	return result;
-}
-
-var memo = {};
-function c(x) {
-	if (!memo[x]) {
-		var combinations = [];
-		coins.forEach(function(coin) {
-			if (coin == x) {
-				combinations.push([coin]);
-			}
-			if (coin < x) {
-				c(x - coin).forEach(function(combination) {
-					combinations.push([coin].concat(combination));
-				});
-			}
-		});
-		memo[x] = unique(combinations);
-	}
-	return memo[x];
-}
+console.log(findCombinationsCount(200, coins));

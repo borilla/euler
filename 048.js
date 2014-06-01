9db0634f73b687305fccc15f8de7fdd46d161a4a
John Adams <github@borilla.co.uk>
2014-06-01 23:08:53 +0100

    Problem 48

---
+++ 048.js
@@ -0,0 +1,12 @@
+
+// limit calculations to just final 10 digits
+BigNum.limit = 10;
+
+var sum = new BigNum(0);
+for (var i = 1; i <= 1000; ++i) {
+	var add = new BigNum(i);
+	add.pow(i);
+	sum.add(add);
+}
+
+console.log('sum', sum);

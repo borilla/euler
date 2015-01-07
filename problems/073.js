39da06dc7dbbe80edc91262d1b936514bb3eb812
John Adams <github@borilla.co.uk>
2015-01-08 00:47:52 +0800

    Problem 73

--- problems/073.js
+++ problems/073.js
@@ -1,56 +1,27 @@
-function fractionsInRange(min, max, d) {
-	var nStart = Math.floor(min.n * d / min.d) + 1;
-	var nEnd = Math.ceil(max.n * d / max.d) - 1;
-	var fractions = [];
-	for (var n = nStart; n <= nEnd; ++n) {
-		if (!isExcluded()) {
-			fractions.push({
-				n: n,
-				d: d
-			});
-		}
-	}
-	return fractions;
-
-	function isExcluded() {
-		for (var i = 2; i < d / 2; ++i) {
-			if (i * n * d == 0) {
-				return true;
-			}
-		}
-		return false;
-	}
+function countFractions(min, max, denom) {
+	var nStart = Math.floor(min.n * denom / min.d) + 1;
+	var nEnd = Math.ceil(max.n * denom / max.d);
+	return nEnd - nStart;
 }
 
-function getAllFractions(min, max, maxD) {
-	var allFractions = [];
-	for (var d = 2; d <= maxD; ++d) {
-		var fractions = fractionsInRange(min, max, d);
-		allFractions = allFractions.concat(fractions);
+function countAllFractions(min, max, maxDenom) {
+	var counts = [0];
+	for (var denom = 1; denom <= maxDenom; ++denom) {
+		counts[denom] = countFractions(min, max, denom);
 	}
-	allFractions.sort(sortFractions);
-	return allFractions;
 
-	function sortFractions(f1, f2) {
-		var v1 = f1.n * f2.d;
-		var v2 = f2.n * f1.d;
-		return v1 == v2 ? 0 : v1 < v2 ? -1 : 1;
+	for (var i = 1; i < maxDenom; ++i) {
+		var count = counts[i];
+		for (var j = i + i; j <= maxDenom; j += i) {
+			counts[j] -= count;
+		}
 	}
+
+	return Utils.arraySum(counts);
 }
 
 var min = { n: 1, d: 3 };
 var max = { n: 1, d: 2 };
 
-console.log('fractionsInRange(min, max, 5)');
-console.table(fractionsInRange(min, max, 5));
-console.log('fractionsInRange(min, max, 8)');
-console.table(fractionsInRange(min, max, 8));
-
-console.log('getAllFractions(min, max, 8)');
-console.table(getAllFractions(min, max, 8));
-
-console.log('getAllFractions(min, max, 16)');
-console.table(getAllFractions(min, max, 16));
-
-console.log('getAllFractions(min, max, 12000).length');
-console.log(getAllFractions(min, max, 12000).length); // 7295372
+console.log('countAllFractions(min, max, 8)', countAllFractions(min, max, 8));
+console.log('countAllFractions(min, max, 12000)', countAllFractions(min, max, 12000));
